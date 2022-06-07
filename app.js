f4590bb9130fad69e5804ded2d727aea02ba4715
nathanjcheng <nathancheng7@gmail.com>
2022-06-06 21:40:41 -0400

    lol

---
+++ app.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container')
+const div = document.querySelector('div')
+
+pixel.addEventListener('click', function () {
+    div.style.backgroundColor = 000000;
+})
